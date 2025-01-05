bafd08025c9f62dff0f2ef051d221d44749a5751
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-05 16:03:05 +0530

    Add Task component and integrate into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Task from './components/task/Task'
 
 function App() {
 
@@ -6,13 +7,20 @@ function App() {
   return (
     <>
       <h1 className='text-white text-center text-4xl font-semibold mt-9 mb-11'>TODO App</h1>
-      <div className='max-w-[1100px] m-auto flex items-center justify-center'>
+      <div className='max-w-[1100px] m-auto flex flex-col items-center justify-center'>
 
         <div className='w-[85vw] md:w-[50vw] flex items-center justify-between'>
           <input type="text" className='w-[75%] rounded pl-7 pr-5 font-semibold outline-none border-none h-9' placeholder='Enter task here...' />
           <button className='w-[20%] bg-orange-600 rounded-sm font-semibold h-9 text-white text-lg'>Add</button>
 
         </div>
+        <div className="text-white my-12 w-[85vw] flex flex-col md:w-[50vw]">
+          <Task />
+          <Task />
+
+        </div>
+
+
 
 
       </div>
---
+++ src/components/task/Task.jsx
@@ -0,0 +1,19 @@
+function Task(params) {
+
+    return (
+        <>
+
+            <div className="flex items-center justify-between bg-[#2c2c2c] mb-6 h-12 rounded">
+                <p className="ml-7">hello hii </p>
+                <button className='w-[15%] bg-orange-600 rounded-sm font-semibold h-9 text-white text-lg mr-3'>Delete</button>
+                {/* <button className='w-[15%] bg-orange-600 rounded-sm font-semibold h-9 text-white text-lg mr-3'>Delete</button> */}
+            </div>
+
+
+        </>
+
+    )
+
+}
+
+export default Task;
