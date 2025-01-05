7f30a6e4d61103138af336e253fa2608a2114df9
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-05 15:23:40 +0530

    Add input and add button

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ function App() {
 
   return (
     <>
+      <h1 className='text-white text-center text-4xl font-semibold mt-9 mb-11'>TODO App</h1>
+      <div className='max-w-[1100px] m-auto flex items-center justify-center'>
+
+        <div className='w-[85vw] md:w-[50vw] flex items-center justify-between'>
+          <input type="text" className='w-[75%] rounded pl-7 pr-5 font-semibold outline-none border-none h-9' placeholder='Enter task here...' />
+          <button className='w-[20%] bg-orange-600 rounded-sm font-semibold h-9 text-white text-lg'>Add</button>
+
+        </div>
+
+
+      </div>
+
     </>
   )
 }
