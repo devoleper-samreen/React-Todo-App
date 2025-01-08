1d717ec037c7dbcaf7f28795a99826d061538466
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-08 16:18:36 +0530

    Add task update and delete functionality with form submission

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import './App.css'
 import Task from './components/task/Task'
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
 
   useEffect(() => {
-    const todos = localStorage.getItem("todos");
-    if (todos) {
-      setTodos(JSON.parse(todos));
+    console.log("loading data from localstorage...")
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+
+    if (savedTodos) {
+      setTodos(savedTodos);
     }
   }, []);
 
   useEffect(() => {
-    console.log("Todos Updated:", todos)
+    console.log("saving data to localstorage...")
+
     localStorage.setItem("todos", JSON.stringify(todos));
 
   }, [todos])
@@ -23,9 +27,10 @@ function App() {
     setInputValue(e.target.value);
   }
 
-  const addTask = () => {
-    if (inputValue.trim()) {
-      setTodos([...todos, inputValue.trim()]);
+  const addTask = (e) => {
+    e.preventDefault();
+    if (inputValue.trim() != "") {
+      setTodos([...todos, inputValue]);
       setInputValue("");
     }
   }
@@ -36,16 +41,17 @@ function App() {
       <h1 className='text-white text-center text-4xl font-semibold mt-9 mb-11'>TODO App</h1>
       <div className='max-w-[1100px] m-auto flex flex-col items-center justify-center'>
 
-        <div className='w-[85vw] md:w-[50vw] flex items-center justify-between'>
+        <form onSubmit={addTask} className='w-[85vw] md:w-[50vw] flex items-center justify-between'>
           <input type="text" className='w-[75%] rounded pl-7 pr-5 font-semibold outline-none border-none h-9' placeholder='Enter task here...' value={inputValue} onChange={handleInputChange} />
-          <button className='w-[20%] bg-orange-600 rounded-sm font-semibold h-9 text-white text-lg' onClick={addTask}>Add</button>
+          <button type="submit" className='w-[20%] bg-orange-600 rounded-sm font-semibold h-9 text-white text-lg'>Add</button>
 
-        </div>
+        </form>
         <div className="text-white my-12 w-[85vw] flex flex-col md:w-[50vw]">
 
           {
             todos.map((todo, idx) => (
-              <Task task={todo} key={idx} />
+              <Task task={todo} key={idx} idx={idx} setTodos={setTodos} todos={todos} />
+
             ))
           }
 
--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,13 +1,31 @@
-function Task({ task }) {
+function Task({ task, idx, todos, setTodos }) {
+
+    const handleDelete = () => {
+        const updatedTodos = todos.filter((_, index) => index !== idx);
+        setTodos(updatedTodos);
+    };
+
+    const handleUpdate = () => {
+        const updatedValue = prompt("enter updated value")
+        console.log(updatedValue)
+
+        if (updatedValue != null && updatedValue.trim() != "") {
+            const editedTodos = todos.map((todo, index) => index === idx ? updatedValue : todo)
+
+            setTodos(editedTodos);
+        }
+
+
+    }
+
 
     return (
         <>
-
             <div className="flex items-center justify-between bg-[#2c2c2c] mb-6 h-12 rounded">
                 <p className="ml-7">{task}</p>
                 <div className="p-1">
-                    <button className='px-5 mr-3 bg-red-700 rounded font-semibold h-9 text-white'>U</button>
-                    <button className='px-5 bg-red-700 rounded font-semibold h-9 text-white'>D</button>
+                    <button className='px-5 mr-3 bg-red-700 rounded font-semibold h-9 text-white' onClick={handleUpdate}>U</button>
+                    <button className='px-5 bg-red-700 rounded font-semibold h-9 text-white' onClick={handleDelete}>D</button>
 
                 </div>
 
