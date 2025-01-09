1fde0ba38780b370135d45e2770a048bd1d0097f
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-09 12:00:10 +0530

    Add toast notifications and auto scroll task div

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,46 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import Task from './components/task/Task'
+import { ToastContainer, toast } from 'react-toastify'
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos"))
+  );
   const [inputValue, setInputValue] = useState("");
-
+  const todoContainerRef = useRef(null);
 
   useEffect(() => {
-    console.log("loading data from localstorage...")
-    const savedTodos = JSON.parse(localStorage.getItem("todos"));
-
-    if (savedTodos) {
-      setTodos(savedTodos);
+    if (todoContainerRef.current) {
+      todoContainerRef.current.scrollTop = todoContainerRef.current.scrollHeight;
     }
-  }, []);
+  }, [todos]);
+
 
   useEffect(() => {
-    console.log("saving data to localstorage...")
-
     localStorage.setItem("todos", JSON.stringify(todos));
 
   }, [todos])
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  }
-
   const addTask = (e) => {
     e.preventDefault();
+
+    if (todos.includes(inputValue.trim())) {
+      toast.warn('Task Already Exist!', {
+        autoClose: 2000,
+      })
+
+      setInputValue("")
+
+      return
+
+    }
     if (inputValue.trim() != "") {
       setTodos([...todos, inputValue]);
       setInputValue("");
+      toast.success('Task Added !', {
+        autoClose: 2000,
+      })
     }
   }
 
@@ -42,11 +51,12 @@ function App() {
       <div className='max-w-[1100px] m-auto flex flex-col items-center justify-center'>
 
         <form onSubmit={addTask} className='w-[85vw] md:w-[50vw] flex items-center justify-between'>
-          <input type="text" className='w-[75%] rounded pl-7 pr-5 font-semibold outline-none border-none h-9' placeholder='Enter task here...' value={inputValue} onChange={handleInputChange} />
+          <input type="text" className='w-[75%] rounded pl-7 pr-5 font-semibold outline-none border-none h-9' placeholder='Enter task here...' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
           <button type="submit" className='w-[20%] bg-orange-600 rounded-sm font-semibold h-9 text-white text-lg'>Add</button>
+          <ToastContainer theme="dark" />
 
         </form>
-        <div className="text-white my-12 w-[85vw] flex flex-col md:w-[50vw]">
+        <div ref={todoContainerRef} className="task-div text-white mt-12 w-[85vw] flex flex-col md:w-[50vw] max-h-[72vh] md:max-h-[360px] overflow-y-auto">
 
           {
             todos.map((todo, idx) => (
--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,8 +1,17 @@
+import { MdDelete } from "react-icons/md";
+import { FiEdit } from "react-icons/fi";
+import { toast } from 'react-toastify';
+
+
 function Task({ task, idx, todos, setTodos }) {
 
     const handleDelete = () => {
         const updatedTodos = todos.filter((_, index) => index !== idx);
         setTodos(updatedTodos);
+
+        toast.info("Delete Task", {
+            autoClose: 2000,
+        })
     };
 
     const handleUpdate = () => {
@@ -13,6 +22,9 @@ function Task({ task, idx, todos, setTodos }) {
             const editedTodos = todos.map((todo, index) => index === idx ? updatedValue : todo)
 
             setTodos(editedTodos);
+            toast.success("Task edit successfully !", {
+                autoClose: 2000,
+            })
         }
 
 
@@ -21,11 +33,12 @@ function Task({ task, idx, todos, setTodos }) {
 
     return (
         <>
-            <div className="flex items-center justify-between bg-[#2c2c2c] mb-6 h-12 rounded">
-                <p className="ml-7">{task}</p>
-                <div className="p-1">
-                    <button className='px-5 mr-3 bg-red-700 rounded font-semibold h-9 text-white' onClick={handleUpdate}>U</button>
-                    <button className='px-5 bg-red-700 rounded font-semibold h-9 text-white' onClick={handleDelete}>D</button>
+            <div className="flex items-center justify-between bg-[#2c2c2c] mb-6 h-14 rounded">
+                <p className="ml-7 font-serif tracking-wider">{task}</p>
+                <div className="p-1 text-center mb-0 mt-[6px]">
+
+                    <button className='px-4 mr-3 mb-0 bg-red-700 rounded font-semibold h-8 text-white' onClick={handleUpdate}><FiEdit /></button>
+                    <button className='px-4 bg-red-700 rounded font-semibold h-8 text-white mr-2 text-xl' onClick={handleDelete}><MdDelete /></button>
 
                 </div>
 
